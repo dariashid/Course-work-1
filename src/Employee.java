6cce1e63c1b7d70d6ebae8417d8db693b72f5825
dariashid <dariashid@mail.ru>
2024-01-27 20:30:32 +0300

    Course work

---
+++ src/Employee.java
@@ -0,0 +1,35 @@
+public class Employee {
+    private String fullName;
+    private int departmentName;
+    private int salary;
+    private int id;
+
+    public Employee (String fullName, int departmentName, int salary, int id){
+        this.fullName = fullName;
+        this.departmentName = departmentName;
+        this.salary = salary;
+        this.id = Main.counter++;
+
+    }
+    public String getFullName () {
+
+        return fullName;
+    }
+
+    public int getDepartmentName() {
+
+        return departmentName;
+    }
+
+    public int getSalary() {
+
+        return salary;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public String toString () {return fullName + " " + departmentName + " " + salary;}
+}
